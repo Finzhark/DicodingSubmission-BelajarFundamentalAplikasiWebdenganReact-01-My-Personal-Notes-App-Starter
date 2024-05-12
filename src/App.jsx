8260a8948f4272870073382bf297ff5376542e6a
Finzhark <126471175+Finzhark@users.noreply.github.com>
2024-05-12 11:30:08 +0700

    Menambah link ke github

--- src/App.jsx
+++ src/App.jsx
@@ -29,7 +29,7 @@ function App() {
       </main>
       <footer>
         <hr/>
-        <p>Dicoding Submission - Belajar Fundamental Aplikasi Web dengan React &#169; 2024, Finzhark</p>
+        <p>Dicoding Submission - Belajar Fundamental Aplikasi Web dengan React &#169; 2024, <Link to='https://github.com/finzhark'>Finzhark</Link></p>
       </footer>
     </div>
   );
